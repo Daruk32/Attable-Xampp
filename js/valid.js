80201259120f9c1e65bd4be85103970f450d088b
Daruk32 <johan.giroux.pro@protonmail.com>
2021-06-01 12:12:44 +0200

    Contrôle Mdp
    Vérification des caractères

--- js/valid.js
+++ js/valid.js
@@ -1,3 +1,4 @@
+/*
 //Fonction pour le formulaire de connection avec conservation des informations sous forme de cookies
 function verif_form_connexion() {
  var mail = document.getElementById("email");
@@ -69,4 +70,77 @@ function verif_form_modification() {
  createCookie("mail",email.value,"20");
  createCookie("mdp",mdp.value,"20");
 
- } 
+}
+*/
+
+function validate_password() {
+  var myInput = document.getElementById("mdp");
+  var letter = document.getElementById("letter");
+  var capital = document.getElementById("capital");
+  var number = document.getElementById("number");
+  var special = document.getElementById("special");
+  var length = document.getElementById("length");
+  
+  // When the user clicks on the password field, show the message box
+  myInput.onfocus = function() {
+    document.getElementById("message").style.display = "block";
+  }
+  
+  // When the user clicks outside of the password field, hide the message box
+  myInput.onblur = function() {
+    document.getElementById("message").style.display = "none";
+  }
+  
+  // When the user starts to type something inside the password field
+  myInput.onkeyup = function() {
+    // Validate lowercase letters
+    var lowerCaseLetters = /[a-z]/g;
+    if(myInput.value.match(lowerCaseLetters)) {  
+      letter.classList.remove("invalid");
+      letter.classList.add("valid");
+    } else {
+      letter.classList.remove("valid");
+      letter.classList.add("invalid");
+    }
+    
+    // Validate capital letters
+    var upperCaseLetters = /[A-Z]/g;
+    if(myInput.value.match(upperCaseLetters)) {  
+      capital.classList.remove("invalid");
+      capital.classList.add("valid");
+    } else {
+      capital.classList.remove("valid");
+      capital.classList.add("invalid");
+    }
+  
+    // Validate numbers
+    var numbers = /[0-9]/g;
+    if(myInput.value.match(numbers)) {  
+      number.classList.remove("invalid");
+      number.classList.add("valid");
+    } else {
+      number.classList.remove("valid");
+      number.classList.add("invalid");
+    }
+
+    // Validate specials
+    var specials = /[^a-zA-Z\d]/g;
+    if(myInput.value.match(specials)) {  
+      special.classList.remove("invalid");
+      special.classList.add("valid");
+    } else {
+      special.classList.remove("valid");
+      special.classList.add("invalid");
+    }    
+
+    // Validate length
+    if(myInput.value.length >= 6) {
+      length.classList.remove("invalid");
+      length.classList.add("valid");
+    } else {
+      length.classList.remove("valid");
+      length.classList.add("invalid");
+    }
+  }
+} 
+
