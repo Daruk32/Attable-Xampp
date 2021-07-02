406db9d1e55265a59d9fde9afa0832b20dc8fc53
Daruk32 <johan.giroux.pro@protonmail.com>
2021-07-02 09:19:18 +0200

    Correctif B Cookie

--- js/cookie.js
+++ js/cookie.js
@@ -5,7 +5,7 @@ function createCookie(name,value,days) {
 		var expires = "; expires="+date.toGMTString();
 	}
 	else var expires = "";
-	document.cookie = name+"="+value+expires+"; path=/"+'; sameSite=Strict;secure=TRUE;';
+	document.cookie = name+"="+value+expires+"; path=/"+';';
 }
 
 
