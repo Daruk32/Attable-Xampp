59814e312d1c8f64a0aa97e9c0a00937b879d83e
Daruk32 <johan.giroux.pro@protonmail.com>
2021-09-03 16:07:54 +0200

    Jtable 2
    CSS & fonctionnel

--- jtable.php
+++ jtable.php
@@ -7,17 +7,21 @@
 		
 		<title>JTable</title>
 
-		<link rel="stylesheet" href="css\attable.css">
+		<link rel="stylesheet" href="css\booststrap.css">
 		<link rel="stylesheet" href="css\footer.css">
 
 		<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous">
 
  
 		<!-- Bootstrap core JS-->
+		<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+		<script src="https://unpkg.com/tableexport.jquery.plugin/tableExport.min.js"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
 		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
 		<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
-		<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
 		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+
 		
 		<!-- icons-->
 		<script src="https://use.fontawesome.com/2e4ae48f29.js"></script>
@@ -32,16 +36,12 @@
 
 		<script src="js/panier.js" type="module"></script>
 
-		<script src="https://unpkg.com/tableexport.jquery.plugin/tableExport.min.js"></script>
 		<script src="https://unpkg.com/bootstrap-table@1.18.3/dist/bootstrap-table.min.js"></script>
 		<script src="https://unpkg.com/bootstrap-table@1.18.3/dist/bootstrap-table-locale-all.min.js"></script>
 		<script src="https://unpkg.com/bootstrap-table@1.18.3/dist/extensions/export/bootstrap-table-export.min.js"></script>
 		<script src="https://unpkg.com/bootstrap-table@1.18.3/dist/extensions/print/bootstrap-table-print.min.js"></script>
 
-
-
-
-
+		<script src="js/data.js" type="module"></script>
 
 	</head>
 	
@@ -76,117 +76,44 @@
 			</nav>			  
 		</section>
 
-<!-- 2 tableaux avec html/Javascript différents à mettre/enlever en commentaires pour ne pas qu'ils s'empiètent. -->
 
-<!-- Tableau 1 à base de données fixes, voir le script plus bas -->
-		<table id="table">
-			<thead>
-				<tr>
-				<th data-field="id">ID</th>
-				<th data-field="libelle">Nom</th>
-				<th data-field="prix">Prix</th>
-				<th data-field="quantity">Quantité</th>
-				<th data-field="categorie">Catégorie</th>
-				</tr>
-			</thead>
-		</table>
-
-
-<!-- Tableau Table avec fonctionnalités, voir sa génération avec le fichier jtable.js -->
-
-<!--
 		<div class="select">
 			<select class="form-control" id="locale">
-			  <option value="af-ZA">af-ZA</option>
-			  <option value="ar-SA">ar-SA</option>
-			  <option value="ca-ES">ca-ES</option>
-			  <option value="cs-CZ">cs-CZ</option>
-			  <option value="da-DK">da-DK</option>
-			  <option value="de-DE">de-DE</option>
-			  <option value="el-GR">el-GR</option>
-			  <option value="en-US" selected>en-US</option>
-			  <option value="es-AR">es-AR</option>
-			  <option value="es-CL">es-CL</option>
-			  <option value="es-CR">es-CR</option>
-			  <option value="es-ES">es-ES</option>
-			  <option value="es-MX">es-MX</option>
-			  <option value="es-NI">es-NI</option>
-			  <option value="es-SP">es-SP</option>
-			  <option value="et-EE">et-EE</option>
-			  <option value="eu-EU">eu-EU</option>
-			  <option value="fa-IR">fa-IR</option>
-			  <option value="fi-FI">fi-FI</option>
-			  <option value="fr-BE">fr-BE</option>
-			  <option value="fr-FR">fr-FR</option>
-			  <option value="he-IL">he-IL</option>
-			  <option value="hr-HR">hr-HR</option>
-			  <option value="hu-HU">hu-HU</option>
-			  <option value="id-ID">id-ID</option>
-			  <option value="it-IT">it-IT</option>
-			  <option value="ja-JP">ja-JP</option>
-			  <option value="ka-GE">ka-GE</option>
-			  <option value="ko-KR">ko-KR</option>
-			  <option value="ms-MY">ms-MY</option>
-			  <option value="nb-NO">nb-NO</option>
-			  <option value="nl-NL">nl-NL</option>
-			  <option value="pl-PL">pl-PL</option>
-			  <option value="pt-BR">pt-BR</option>
-			  <option value="pt-PT">pt-PT</option>
-			  <option value="ro-RO">ro-RO</option>
-			  <option value="ru-RU">ru-RU</option>
-			  <option value="sk-SK">sk-SK</option>
-			  <option value="sv-SE">sv-SE</option>
-			  <option value="th-TH">th-TH</option>
-			  <option value="tr-TR">tr-TR</option>
-			  <option value="uk-UA">uk-UA</option>
-			  <option value="ur-PK">ur-PK</option>
-			  <option value="uz-Latn-UZ">uz-Latn-UZ</option>
-			  <option value="vi-VN">vi-VN</option>
-			  <option value="zh-CN">zh-CN</option>
-			  <option value="zh-TW">zh-TW</option>
+				<option value="en-US">en-US</option>
+				<option value="fr-FR" selected>fr-FR</option>
 			</select>
 		</div>
 
+
 		<div id="toolbar">
 			<button id="remove" class="btn btn-danger" disabled>
-			  <i class="fa fa-trash"></i> Delete
+				<i class="fa fa-trash"></i> Delete
 			</button>
 		</div>
 		<table
-			id="table"
-			data-toolbar="#toolbar"
-			data-search="true"
-			data-show-refresh="true"
-			data-show-toggle="true"
-			data-show-fullscreen="true"
-			data-show-columns="true"
-			data-show-columns-toggle-all="true"
-			data-show-print="true"
-			data-detail-view="true"
-			data-show-export="true"
-			data-click-to-select="true"
-			data-detail-formatter="detailFormatter"
-			data-minimum-count-columns="2"
-			data-show-pagination-switch="true"
-			data-pagination="true"
-			data-id-field="id"
-			data-page-list="[10, 25, 50, 100, all]"
-			data-show-footer="true"
-			data-side-pagination="server"
-			data-url="donnees.json"
-			data-response-handler="responseHandler">
-			
-			<thead>
-				<tr>
-				  <th data-field="name">Name</th>
-				  <th data-field="follower">Follower</th>
-				  <th data-field="following">Following</th>
-				  <th data-field="snippets">Snippets</th>
-				</tr>
-			</thead>
+		id="table"
+		data-toolbar="#toolbar"
+		data-search="true"
+		data-show-refresh="true"
+		data-show-toggle="true"
+		data-show-print="true"
+		data-show-fullscreen="true"
+		data-show-columns="true"
+		data-show-columns-toggle-all="true"
+		data-detail-view="true"
+		data-show-export="true"
+		data-click-to-select="true"
+		data-detail-formatter="detailFormatter"
+		data-minimum-count-columns="2"
+		data-show-pagination-switch="true"
+		data-pagination="true"
+		data-id-field="id"
+		data-page-list="[10, 25, 50, 100, all]"
+		data-show-footer="true"
+		data-side-pagination="server"
+		data-url="bddproducts.json"
+		data-response-handler="responseHandler">
 		</table>
--->
-
 
 
 
@@ -221,152 +148,144 @@
 			</div>
         </footer>
 
+		
+		<script>
+		var $table = $('#table')
+		var $remove = $('#remove')
+		var selections = []
+
+		function getIdSelections() {
+			return $.map($table.bootstrapTable('getSelections'), function (row) {
+			return row.id
+			})
+		}
+
+		function responseHandler(res) {
+			$.each(res.rows, function (i, row) {
+			row.state = $.inArray(row.id, selections) !== -1
+			})
+			return res
+		}
+
+		function detailFormatter(index, row) {
+			var html = []
+			$.each(row, function (key, value) {
+			html.push('<p><b>' + key + ':</b> ' + value + '</p>')
+			})
+			return html.join('')
+		}
+
+		function operateFormatter(value, row, index) {
+			return [
+			'<a class="like" href="javascript:void(0)" title="Like">',
+			'<i class="fa fa-heart"></i>',
+			'</a>  ',
+			'<a class="remove" href="javascript:void(0)" title="Remove">',
+			'<i class="fa fa-trash"></i>',
+			'</a>'
+			].join('')
+		}
+
+		window.operateEvents = {
+			'click .like': function (e, value, row, index) {
+			alert('You click like action, row: ' + JSON.stringify(row))
+			},
+			'click .remove': function (e, value, row, index) {
+			$table.bootstrapTable('remove', {
+				field: 'id',
+				values: [row.id]
+			})
+			}
+		}
+
+		function totalTextFormatter(data) {
+			return 'Total'
+		}
+
+		function totalNameFormatter(data) {
+			return data.length
+		}
 
 
-<script>
-  var $table = $('#table')
 
-  $(function() {
-    var data = [
-        {
-    "url": "categorie/promotions/promo2.png",
-    "libelle": "Crispeas en promotion 1€ les 2",
-    "prix": 1,
-    "texte": "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
-    "quantity": 0,
-    "id": "01",
-    "categorie": "Promotions"
-},
-{
-    "url": "categorie/promotions/promo3.png",
-    "libelle": "Miel en promotion 3€ 500g 5€ 2x500g",
-    "prix": 3,
-    "texte": "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
-    "quantity": 0,
-    "id": "02",
-    "categorie": "Promotions"
-},
-{
-    "url": "categorie/promotions/promo4.png",
-    "libelle": "Promotion sur tout le rayon gâteau en lot !",
-    "prix": 1,
-    "texte": "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
-    "quantity": 0,
-    "id": "03",
-    "categorie": "Promotions"
-},
-{
-    "url": "categorie/promotions/promo5.png",
-    "libelle": "promotion sur une multitude de produits en lot !",
-    "prix": 3,
-    "texte": "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
-    "quantity": 0,
-    "id": "04",
-    "categorie": "Promotions"
-},
-{
-    "url": "categorie/boissons/boisson2.png",
-    "libelle": "Jus pomme orange et boissons gazeuses",
-    "prix": 1,
-    "texte": "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
-    "quantity": 0,
-    "id": "10000",
-    "categorie": "Boissons"
-},
-{
-    "url": "categorie/boissons/boisson3.png",
-    "libelle": "Fanta Perrier et Sanpellegrino",
-    "prix": 1,
-    "texte": "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
-    "quantity": 0,
-    "id": "10001",
-    "categorie": "Boissons"
-},
-{
-    "url": "categorie/boissons/boisson4.png",
-    "libelle": "Jus pomme orange et boissons gazeuses",
-    "prix": 1,
-    "texte": "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
-    "quantity": 0,
-    "id": "10002",
-    "categorie": "Boissons"
-},
-{
-    "url": "categorie/boissons/boisson5.png",
-    "libelle": "Fanta Perrier et Sanpellegrino",
-    "prix": 1,
-    "texte": "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
-    "quantity": 0,
-    "id": "10003",
-    "categorie": "Boissons"
-},
-{
-    "url": "categorie/boissons/boisson6.png",
-    "libelle": "Essai Prix 0",
-    "prix": 0,
-    "texte": "Un test pour voir",
-    "quantity": 0,
-    "id": "10004",
-    "categorie": "Boissons"
-},
-{
-    "url": "categorie/boissons/fanta.jpg",
-    "libelle": "Fanta",
-    "prix": 0.5,
-    "texte": "Une boisson râfraichissante",
-    "quantity": 0,
-    "id": "10005",
-    "categorie": "Boissons"
-},
-{
-    "url": "categorie/bonbons/bonbons1.png",
-    "libelle": "Ribambel de bonbons",
-    "prix": 1,
-    "texte": "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
-    "quantity": 0,
-    "id": "20000",
-    "categorie": "Bonbons"
-},
-{
-    "url": "categorie/bonbons/bonbons2.png",
-    "libelle": "bonbons et barres chocolatés",
-    "prix": 0.1,
-    "texte": "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
-    "quantity": 0,
-    "id": "20001",
-    "categorie": "Bonbons"
-},
-{
-    "url": "categorie/bonbons/bonbons3.png",
-    "libelle": "bonbon au chocolat",
-    "prix": 1,
-    "texte": "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
-    "quantity": 0,
-    "id": "20002",
-    "categorie": "Bonbons"
-},
-{
-    "url": "categorie/bonbons/bonbons4.png",
-    "libelle": "Ribambel de bonbons",
-    "prix": 1,
-    "texte": "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
-    "quantity": 0,
-    "id": "20003",
-    "categorie": "Bonbons"
-},
-{
-    "url": "categorie/bonbons/bonbons5.png",
-    "libelle": "bonbons et barres chocolatés",
-    "prix": 0.1,
-    "texte": "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
-    "quantity": 0,
-    "id": "20004",
-    "categorie": "Bonbons"
-}
-]
-    $table.bootstrapTable({data: data})
-  })
-</script>
+		function initTable() {
+			$table.bootstrapTable('destroy').bootstrapTable({
+			height: 550,
+			locale: $('#locale').val(),
+			columns: [
+				[{
+				field: 'state',
+				checkbox: true,
+				rowspan: 2,
+				align: 'center',
+				valign: 'middle'
+				}, {
+				title: 'Référence',
+				field: 'id',
+				rowspan: 2,
+				align: 'center',
+				valign: 'middle',
+				sortable: true,
+				footerFormatter: totalTextFormatter
+				}, {
+				title: 'Item Detail',
+				colspan: 4,
+				align: 'center'
+				}],
+				[{
+				field: 'libelle',
+				title: 'Nom',
+				sortable: true,
+				footerFormatter: totalNameFormatter,
+				align: 'center'
+				}, {
+				field: 'prix',
+				title: 'Prix',
+				sortable: true,
+				align: 'center',
+				}, {
+				field: 'quantity',
+				title: 'Quantité',
+				sortable: true,
+				align: 'center',
+				}, {
+				field: 'operate',
+				title: 'Item Operate',
+				align: 'center',
+				clickToSelect: false,
+				events: window.operateEvents,
+				formatter: operateFormatter
+				}]
+			]
+			})
+			$table.on('check.bs.table uncheck.bs.table ' +
+			'check-all.bs.table uncheck-all.bs.table',
+			function () {
+			$remove.prop('disabled', !$table.bootstrapTable('getSelections').length)
+
+			// save your data, here just save the current page
+			selections = getIdSelections()
+			// push or splice the selections if you want to save all data selections
+			})
+			$table.on('all.bs.table', function (e, name, args) {
+			console.log(name, args)
+			})
+			$remove.click(function () {
+			var ids = getIdSelections()
+			$table.bootstrapTable('remove', {
+				field: 'id',
+				values: ids
+			})
+			$remove.prop('disabled', true)
+			})
+		}
+
+		$(function() {
+			initTable()
+
+			$('#locale').change(initTable)
+		})
+		</script>
 
 	</body>
 </html>
--- testjson.php
+++ testjson.php
@@ -8,9 +8,10 @@
 		<title>Accueil</title>
 
 		<link rel="stylesheet" href="css\attable.css">
+		<link rel="stylesheet" href="css\booststrap.css">
 		<link rel="stylesheet" href="css\footer.css">
  
-		<!-- Bootstrap core JS - MAJ FOCNTIONNELLE-->
+		<!-- Bootstrap core JS - MAJ FONCTIONNELLE-->
 		<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
 		<script src="https://unpkg.com/tableexport.jquery.plugin/tableExport.min.js"></script>
@@ -33,184 +34,21 @@
 	
 		<section>
 
-			<!--
 			<table
-				id="table"
-				>
-				<thead>
-					<tr>
-						<th data-field="id">ID</th>
-						<th data-field="libelle">Item Name</th>
-						<th data-field="prix">Item Price</th>
-					</tr>
-				</thead>
+			id="table"
+			data-toggle="table"
+			data-url="bddproducts.json">
+			<thead>
+				<tr>
+				<th data-field="id">ID</th>
+				<th data-field="libelle">Nom</th>
+				<th data-field="prix">Prix</th>
+				</tr>
+			</thead>
 			</table>
--->
-
-<table
-  id="table"
-  data-toggle="table"
-  data-url="bddproducts.json">
-  <thead>
-    <tr>
-      <th data-field="id">ID</th>
-      <th data-field="libelle">Nom</th>
-      <th data-field="prix">Prix</th>
-    </tr>
-  </thead>
-</table>
-
-
-
-
 
 		</section>
-<!--		
-		<script>
-			var $table = $('#table')
-		  
-			$(function() {
-			  var data = [
-				  {
-			  "url": "categorie/promotions/promo2.png",
-			  "libelle": "Crispeas en promotion 1€ les 2",
-			  "prix": 1,
-			  "texte": "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
-			  "quantity": 0,
-			  "id": "01",
-			  "categorie": "Promotions"
-		  },
-		  {
-			  "url": "categorie/promotions/promo3.png",
-			  "libelle": "Miel en promotion 3€ 500g 5€ 2x500g",
-			  "prix": 3,
-			  "texte": "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
-			  "quantity": 0,
-			  "id": "02",
-			  "categorie": "Promotions"
-		  },
-		  {
-			  "url": "categorie/promotions/promo4.png",
-			  "libelle": "Promotion sur tout le rayon gâteau en lot !",
-			  "prix": 1,
-			  "texte": "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
-			  "quantity": 0,
-			  "id": "03",
-			  "categorie": "Promotions"
-		  },
-		  {
-			  "url": "categorie/promotions/promo5.png",
-			  "libelle": "promotion sur une multitude de produits en lot !",
-			  "prix": 3,
-			  "texte": "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
-			  "quantity": 0,
-			  "id": "04",
-			  "categorie": "Promotions"
-		  },
-		  {
-			  "url": "categorie/boissons/boisson2.png",
-			  "libelle": "Jus pomme orange et boissons gazeuses",
-			  "prix": 1,
-			  "texte": "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
-			  "quantity": 0,
-			  "id": "10000",
-			  "categorie": "Boissons"
-		  },
-		  {
-			  "url": "categorie/boissons/boisson3.png",
-			  "libelle": "Fanta Perrier et Sanpellegrino",
-			  "prix": 1,
-			  "texte": "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
-			  "quantity": 0,
-			  "id": "10001",
-			  "categorie": "Boissons"
-		  },
-		  {
-			  "url": "categorie/boissons/boisson4.png",
-			  "libelle": "Jus pomme orange et boissons gazeuses",
-			  "prix": 1,
-			  "texte": "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
-			  "quantity": 0,
-			  "id": "10002",
-			  "categorie": "Boissons"
-		  },
-		  {
-			  "url": "categorie/boissons/boisson5.png",
-			  "libelle": "Fanta Perrier et Sanpellegrino",
-			  "prix": 1,
-			  "texte": "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
-			  "quantity": 0,
-			  "id": "10003",
-			  "categorie": "Boissons"
-		  },
-		  {
-			  "url": "categorie/boissons/boisson6.png",
-			  "libelle": "Essai Prix 0",
-			  "prix": 0,
-			  "texte": "Un test pour voir",
-			  "quantity": 0,
-			  "id": "10004",
-			  "categorie": "Boissons"
-		  },
-		  {
-			  "url": "categorie/boissons/fanta.jpg",
-			  "libelle": "Fanta",
-			  "prix": 0.5,
-			  "texte": "Une boisson râfraichissante",
-			  "quantity": 0,
-			  "id": "10005",
-			  "categorie": "Boissons"
-		  },
-		  {
-			  "url": "categorie/bonbons/bonbons1.png",
-			  "libelle": "Ribambel de bonbons",
-			  "prix": 1,
-			  "texte": "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
-			  "quantity": 0,
-			  "id": "20000",
-			  "categorie": "Bonbons"
-		  },
-		  {
-			  "url": "categorie/bonbons/bonbons2.png",
-			  "libelle": "bonbons et barres chocolatés",
-			  "prix": 0.1,
-			  "texte": "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
-			  "quantity": 0,
-			  "id": "20001",
-			  "categorie": "Bonbons"
-		  },
-		  {
-			  "url": "categorie/bonbons/bonbons3.png",
-			  "libelle": "bonbon au chocolat",
-			  "prix": 1,
-			  "texte": "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
-			  "quantity": 0,
-			  "id": "20002",
-			  "categorie": "Bonbons"
-		  },
-		  {
-			  "url": "categorie/bonbons/bonbons4.png",
-			  "libelle": "Ribambel de bonbons",
-			  "prix": 1,
-			  "texte": "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
-			  "quantity": 0,
-			  "id": "20003",
-			  "categorie": "Bonbons"
-		  },
-		  {
-			  "url": "categorie/bonbons/bonbons5.png",
-			  "libelle": "bonbons et barres chocolatés",
-			  "prix": 0.1,
-			  "texte": "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
-			  "quantity": 0,
-			  "id": "20004",
-			  "categorie": "Bonbons"
-		  }
-		  ]
-			  $table.bootstrapTable({data: data})
-			})
-		</script>
-		-->
+
 	</body>
 	
 </html>
