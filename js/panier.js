74128171d0046d1e4c39cb30edc62712b9259924
Daruk32 <johan.giroux.pro@protonmail.com>
2021-09-06 17:11:48 +0200

    Maj PDF

--- js/panier.js
+++ js/panier.js
@@ -192,3 +192,28 @@ window.supp = function supp(article) {
 		location.reload();
 	}
 }
+
+
+//Fonction de génération de PDF depuis l'html
+$(document).ready(function(){
+
+	var specialElementHandlers = {
+		"#editor":function(element,renderer){
+			return true;
+		}
+	};
+
+	$("#cmd").click(function(){
+		var doc = new jsPDF();
+		doc.fromHTML($("#main").html(),15,15,{
+			"width":170,
+			"elementHandlers":specialElementHandlers
+		});
+
+		doc.save("sample-file.pdf");
+	});
+});
+
+
+
+
