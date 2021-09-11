0aa73672f7b7284d1e9ff923f57062ced832ffaf
Daruk32 <johan.giroux.pro@protonmail.com>
2021-09-11 10:04:39 +0200

    MAJ Versionning

--- js/addproduct.js
+++ js/addproduct.js
@@ -1,10 +1,10 @@
-function add() {
-    //Prise des datas du produit
+function addProduct() {
+    //Récupération des datas du produit renseigné
     var id = document.getElementById("id").value;
-    var libelle = document.getElementById("libelle").value;
-    var legP1 = document.getElementById("legP1").value;
-    var sleg = document.getElementById("sleg").value;
-    var descrip = document.getElementById("descrip").value;
+    var name = document.getElementById("name").value;
+    var legend_P1 = document.getElementById("legend_P1").value;
+    var short_legend = document.getElementById("short_legend").value;
+    var descriptive = document.getElementById("descriptive").value;
     var price = document.getElementById("price").value;
     var quantity = document.getElementById("quantity").value;
     //var categorie = document.getElementById("categ").value;
@@ -12,17 +12,19 @@ function add() {
     //Mise des datas dans un array
     var new_product = {
         id: id,
-        libelle: libelle,
-        legP1: legP1,
-        sleg: sleg,
-        descrip: descrip,
+        name: name,
+        legend_P1: legend_P1,
+        short_legend: short_legend,
+        descriptive: descriptive,
         price: price,
         quantity: quantity
     };
 
     //Ajout à la Base de données
     var storage = JSON.parse(localStorage.bddproducts);
+    console.log(storage);
     storage.push(new_product);
     var str_json = JSON.stringify(storage);
     localStorage.setItem("bddproducts", str_json);
+    console.log(str_json);
 }
